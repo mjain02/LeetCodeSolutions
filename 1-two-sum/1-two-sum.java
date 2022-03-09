94b3c72ded344712157696037442cada12d5ca06
Meghna Jain <67586580+mjain02@users.noreply.github.com>
2022-03-08 23:16:06 -0500

    Time: 52 ms (35.04%), Space: 44.9 MB (41.44%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -2,8 +2,9 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] sol = new int[2];
         for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
             for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target){
+                if (a + nums[j] == target){
                     sol[0] = i;
                     sol[1] = j;
                 }

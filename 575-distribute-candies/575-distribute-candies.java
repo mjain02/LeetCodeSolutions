c712f912cf02ca0c0d17c20d478cb0b4879717c4
Meghna Jain <67586580+mjain02@users.noreply.github.com>
2022-06-05 16:59:09 -0400

    Time: 53 ms (54.80%), Space: 98.6 MB (55.70%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -2,9 +2,9 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         int candies = candyType.length / 2;
         int currentType = candyType[0];
-        HashMap<Integer, Integer> types = new HashMap<>();
+        HashSet<Integer> types = new HashSet<>();
         for (int i = 0; i < candyType.length; i++) {
-            types.put(candyType[i], i);
+            types.add(candyType[i]);
             if (types.size() > candies)
                 return candies;
         }
